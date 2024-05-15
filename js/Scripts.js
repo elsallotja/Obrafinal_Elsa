b319260c9e45be0bec07e4b1080140de8357342e
elsallotja <m22.erubio@llotja.cat>
2024-05-15 14:10:14 +0200

    Add files via upload

---
+++ js/Scripts.js
@@ -0,0 +1,11 @@
+
+
+function showModal() {
+	document.getElementById("modalComprar").style.display="block";
+	document.body.style.overflow = 'hidden';
+}
+
+function hideModal() {
+	document.getElementById("modalComprar").style.display="none";
+	document.body.style.overflow = 'auto';
+}
